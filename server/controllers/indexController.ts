f7f2f1ca0fbeee50387b0a711284cb6fa77cac95
Joshua <joshuadejesus12345@gmail.com>
2025-12-04 21:54:30 +0800

    added daily macros

--- server/controllers/indexController.ts
+++ server/controllers/indexController.ts
@@ -1,8 +1,19 @@
 import {prisma} from '../prisma.js'
 import type { Request, Response } from 'express'
 
-export async function getMeal(req:Request, res:Response){
-    const meals = await prisma.meal.findMany();
+export async function getMeal(req:Request, res:Response){  
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const meals = await prisma.meal.findMany({
+        where: {
+            date: {
+                gte: today,
+                lt: tomorrow
+        }}
+    }
+    );
     res.json(meals);
     console.log(meals)
 }
