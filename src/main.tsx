62b17e2ff8729fa66ac18f340e3d5368620a7a39
Joshua <joshuadejesus12345@gmail.com>
2025-12-07 18:22:33 +0800

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import { AddMeal } from './components/addMeal.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+        <App />
+  },
+  {
+    path: "/addmeal",
+    element:
+      <AddMeal/>
+  },
+  //Fallback route
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router}  />
   </StrictMode>,
 )
