45a5183b07e8fb42dd62a7be39dbdfd9299e2c07
Joshua <joshuadejesus12345@gmail.com>
2025-12-18 13:34:42 +0800

    removed supabase client

--- server/controllers/addMeal.ts
+++ server/controllers/addMeal.ts
@@ -5,13 +5,13 @@ import type { Request, Response } from 'express'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nhjyqsdirnmpgvidbeeh.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+//const supabaseKey = process.env.SUPABASE_KEY
+//const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default async function addMeal(req:Request, res:Response){
     console.log(req.body);
     const {name, caloriesNum, proteinNum, fatNum, carbsNum } = req.body;
-        await prisma.meal.create({
+        /* await prisma.meal.create({
             data: {
                 name,
                 calories:caloriesNum,
@@ -19,7 +19,8 @@ export default async function addMeal(req:Request, res:Response){
                 fat:fatNum,
                 carbs:carbsNum
             }
-        })
+        })*/
+
         res.json({success: true, message: 'Meal added successfully'});
         console.log('Meal added successfully');
 }       
